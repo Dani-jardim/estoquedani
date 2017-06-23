13d5f453485c8d6eead75a9528341da107ddccd5
Dani-jardim <danialvesdesigner@gmail.com>
2017-06-23 10:20:44 -0300

    Update conexao.php

--- modulo/conexao.php
+++ modulo/conexao.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "us-cdbr-iron-east-03.cleardb.net";
-$username = "bf79b26079404e";
-$password = "e4337512";
-$dbname = "heroku_bb495d296a90024";
+para $servername = "dreamteam.mysql.uhserver.com";
+$username = "dreamteam";
+$password = "L30B[j5}10cqa";
+$dbname = "dreamteam";
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 
