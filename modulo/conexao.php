991951e1562fa6b87b5ec6fa15f8ea33528b735e
Dani-jardim <danialvesdesigner@gmail.com>
2017-06-22 23:36:24 -0300

    Add files via upload

--- modulo/conexao.php
+++ modulo/conexao.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "127.0.0.1";
-$username = "root";
-$password = "";
+$servername = "us-cdbr-iron-east-03.cleardb.net";
+$username = "bf79b26079404e";
+$password = "e4337512";
 $dbname = "heroku_bb495d296a90024";
 
 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
